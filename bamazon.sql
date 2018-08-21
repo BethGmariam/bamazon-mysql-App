75bab5da56b707bab1ed1554f9233ccf5775d352
BethGmariam <36454296+BethGmariam@users.noreply.github.com>
2018-08-21 10:21:36 -0400

    Update bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -6,14 +6,21 @@ USE bamazon;
 
 
 CREATE TABLE products(
---  unique id for each product... TINYINT takes the least memory space and can hold 0-255
-    item_id INTEGER NOT NULL AUTO_INCREMENT,
+    	item_id INTEGER NOT NULL AUTO_INCREMENT,
 	product_name VARCHAR(100),
-    department_name VARCHAR(100),
+    	department_name VARCHAR(100),
 	price DECIMAL(10,2) ,
-    stock_quantity INTEGER,
+    	stock_quantity INTEGER,
 	PRIMARY KEY (item_id)
 );
 
+CREATE TABLE departments(
+    	department_id INTEGER NOT NULL AUTO_INCREMENT,
+	Department_name VARCHAR(100),
+    	over_head_costs DECIMAL(10,2) ,
+	PRIMARY KEY (department_id)
+);
+
 SELECT * FROM products;
+SELECT * FROM departments;
 
